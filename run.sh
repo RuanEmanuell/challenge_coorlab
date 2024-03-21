5dc74a171c79562c001d481bfd0d6baab8f08c81
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-03-21 16:37:56 -0300

    (Version 1.0) Finishing script config

--- run.sh
+++ run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+killall flask
+
 pip install python-dotenv
 pip install Flask
 pip install Flask-Cors
 
+sudo apt install python3-flask
+
 cd server/data
-flask --app server run &
+gnome-terminal -- flask --app server run &
 cd ..
 
 sleep 2
 
 cd files
-flask --app static run &
+gnome-terminal -- flask --app static run
 
 sleep 2
 
-xdg-open http://localhost:8080/client/pages/login/index.html
+firefox http://localhost:8080/client/pages/login/index.html
 
